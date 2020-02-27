a59d00641b0d6b8c5b06f2fc8302c37e33b21c7d
Ras azzab <ras.dinho7@gmail.com>
2020-02-27 03:57:26 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -15,13 +15,15 @@ end
 def literal_aoh
   # Using Array literal syntax only, build a nested array that uses the data in
   # held in the constants
+  [DON_G,JOELLE_VD,PAT_M,KATE_G,BRUCE_G]
 end
 
 def aoh_lookup(aoh, row, key)
-  
+  aoh[row][key]
 end
 
 def aoh_update(aoh, row, key, new_value)
   # Update the AoH data at row and key to have the value of new_value
   # Return the updated AoH
+  aoh[row][key]=new_value
 end
